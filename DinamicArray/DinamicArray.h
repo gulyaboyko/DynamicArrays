97e1b83274cf27dc56300efbe7274e46ab23eb21
Gulya Akhtyamova <gulya.nadym@gmail.com>
2020-06-09 16:02:55 +0300

    Initial Commit

---
+++ DinamicArray/DinamicArray.h
@@ -0,0 +1,19 @@
+//
+//  DinamicArray.h
+//  DinamicArray
+//
+//  Created by Gulya Boiko on 6/9/20.
+//  Copyright © 2020 com.gulya.boiko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DinamicArray.
+FOUNDATION_EXPORT double DinamicArrayVersionNumber;
+
+//! Project version string for DinamicArray.
+FOUNDATION_EXPORT const unsigned char DinamicArrayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DinamicArray/PublicHeader.h>
+
+
